9a0bf421e28f7f56768a8bfd97f7ebcb77162416
juzi3356 <juzi3356@gmail.com>
2020-04-18 08:53:11 +0800

    modify

--- diy.sh
+++ diy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+
 mkdir package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
